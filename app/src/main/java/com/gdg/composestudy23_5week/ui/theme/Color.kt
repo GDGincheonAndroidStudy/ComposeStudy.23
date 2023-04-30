aad15ef22f1ff7a78856dadb41aec73a63e86f57
권혁준 <khjunejune10@gmail.com>
2023-04-30 11:05:51 +0900

    Initial commit

---
+++ app/src/main/java/com/gdg/composestudy23_5week/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.gdg.composestudy23_5week.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
