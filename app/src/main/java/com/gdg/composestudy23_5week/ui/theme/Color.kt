ad1ffd7a9233590e24a9ec7c47ff0eea1d06f969
Holy-Knight <vnycall74@naver.com>
2023-05-06 01:17:52 +0900

    feat: color and theme

--- app/src/main/java/com/gdg/composestudy23_5week/ui/theme/Color.kt
+++ app/src/main/java/com/gdg/composestudy23_5week/ui/theme/Color.kt
@@ -2,6 +2,8 @@ package com.gdg.composestudy23_5week.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val KawaiRed = Color(0xFFF62337)
+val KawaiBlue = Color(0xFF2D99DB)
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
