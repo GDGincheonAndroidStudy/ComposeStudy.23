8fa8c2a9b70dcc0057efa6ae9d92dd24114e6f62
hyuna <akxmcse@gmail.com>
2023-05-07 12:56:32 +0900

    [ADD] data files

---
+++ app/src/main/java/com/gdg/composestudy23_5week/data/Radio.kt
@@ -0,0 +1,10 @@
+package com.gdg.composestudy23_5week.data
+
+data class Radio(
+    val title: String,
+    val description: String,
+    val image: Int,
+    val time: String,
+    val radioTitle: String,
+    val radioDescription: String
+)
---
+++ app/src/main/java/com/gdg/composestudy23_5week/data/Station.kt
@@ -0,0 +1,7 @@
+package com.gdg.composestudy23_5week.data
+
+data class Station(
+    val title: String,
+    val description: String,
+    val image: Int
+)
